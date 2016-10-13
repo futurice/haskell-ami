eea410421f5392419ad4e84333bbd37ec05ce6d4
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-10-13 11:30:08 +0300

    Add docker and postgresql-client

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ set -ex
 case $1 in
 ## Add hvr-ppa
 apt-repos)
-	apt-get -yq update
-    apt-get -yq --no-install-suggests --no-install-recommends --force-yes install software-properties-common python-software-properties
-	apt-add-repository -y "ppa:hvr/ghc" 
-	apt-get update
+    apt-get -yq update
+    apt-get -yq --no-install-suggests --no-install-recommends --force-yes install software-properties-common python-software-properties apt-transport-https ca-certificates
+    apt-add-repository -y "ppa:hvr/ghc"
+    apt-get update
 ;;
 
 # Essential packages
@@ -29,6 +29,7 @@ apt-packages)
         curl \
         git \
         tmux \
+		postgresql-client \
         pkg-config \
         libfftw3-dev \
         liblapack-dev \
@@ -42,11 +43,11 @@ apt-packages)
 # Very minimal vim setup
 vimrc)
 cat > $HOME/.vimrc <<'EOF'
-set nocp                                                                           
-set nu                                                                             
-set nomodeline                                                                     
-set noexpandtab                                                                    
-set nowrap  
+set nocp
+set nu
+set nomodeline
+set noexpandtab
+set nowrap
 set backspace=2
 set whichwrap+=<,>,h,l
 set encoding=utf-8
@@ -89,7 +90,7 @@ ghc-select)
         cabal new-build
         mkdir -p $HOME/.local/bin
         cp `find dist-newstyle/build -name ghc-select -type f | head -n 1` $HOME/.local/bin/
-        $(ghc-select ghc-8.0.1 cabal-head)
+        $($HOME/.local/bin/ghc-select ghc-8.0.1 cabal-head)
         cd $HOME
     fi
 ;;
@@ -100,26 +101,46 @@ hackage-cli)
     if [ ! -d hackage-cli ]; then
         git clone https://github.com/hackage-trustees/hackage-cli.git
         cd hackage-cli
-        $(ghc-select ghc-8.0.1 cabal-head)
+        $($HOME/.local/bin/ghc-select ghc-8.0.1 cabal-head)
         cabal new-build
         cp `find dist-newstyle/build -name hackage-cli -type f | head -n 1` $HOME/.local/bin/
     fi
 ;;
 
 stack)
-    curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack' 
-    stack update
+    curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+    $HOME/.local/bin/stack update
 ;;
 
 packdeps)
     if [ ! -d packdeps ]; then
-        $(ghc-select ghc-8.0.1 cabal-head)
+        $($HOME/.local/bin/ghc-select ghc-8.0.1 cabal-head)
         cabal get packdeps
         mv packdeps-* packdeps
         cd packdeps
         cabal new-build
         cp `find dist-newstyle/build -name packdeps -type f | head -n 1` $HOME/.local/bin/
     fi
+;;
+
+# https://docs.docker.com/engine/installation/linux/ubuntulinux/
+docker)
+    sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+    mkdir -p /etc/apt/sources.list.d/
+    echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+    apt-get update
+    apt-cache policy docker-engine
+    # Allow to use aufs
+    apt-get -yq --no-install-suggests --no-install-recommends --force-yes install linux-image-extra-$(uname -r) linux-image-extra-virtual
+
+    # Install
+    apt-get -yq --no-install-suggests --no-install-recommends --force-yes install docker-engine
+    service docker start || true
+    docker run hello-world
+
+    # Group
+    groupadd docker || true
+    usermod -aG docker ubuntu # $USER is `root`
 ;;
 
 all)
@@ -131,6 +152,7 @@ all)
     sh $0 hackage-cli
     sh $0 stack
     sh $0 packdeps
+    sudo sh $0 docker
 ;;
 
 esac
